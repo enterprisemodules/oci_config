a28591dfedabeef157489cc70f89f0b83fe1e29e
Wim <wimkorevaar@gmail.com>
2024-07-19 14:16:34 +0200

    Checks whether v1 uri is available, if not use v2

--- lib/facter/oci_client_facts.rb
+++ lib/facter/oci_client_facts.rb
@@ -7,13 +7,28 @@ require 'net/http'
 require 'json'
 require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/hash'
 require_relative '../puppet_x/enterprisemodules/oci/monkey_patches/string'
+# Checks whether v1 uri is available, if not use v2.
+def check_uri(uri)
+  begin
+    response = Net::HTTP.get_response(uri)
+    return response.code.to_i == 200
+  rescue StandardError
+    return false
+  end
+end
+
+if check_uri(URI('http://169.254.169.254/opc/v1/instance/'))
+  version = "v1"
+else
+  version = "v2"
+end
 
 def instance_path
-  '/opc/v2/instance/'
+  "/opc/#{version}/instance/"
 end
 
 def vnic_path
-  '/opc/v2/vnics/'
+  "/opc/#{version}/vnics/"
 end
 
 def add_authorization_header(request)
@@ -21,12 +36,16 @@ def add_authorization_header(request)
 end
 
 def get_data(path)
-  request = Net::HTTP::Get.new(URI("http://169.254.169.254#{path}"))
-  add_authorization_header(request)
+  if version == 'v2'
+    request = Net::HTTP::Get.new(URI("http://169.254.169.254#{path}"))
+    add_authorization_header(request)
+  end
   response = Net::HTTP.start('169.254.169.254', 80, :read_timeout => 0.5, :open_timeout => 0.5) do |http|
     http.read_timeout = 0.5
     http.get(path)
-    http.request(request)
+    if version == 'v2'
+      http.request(request)
+    end
   end
   json_data = response.body
   begin
